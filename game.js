34a2e6ea938dad3d68b2327177e4b206fad41a87
MarcosRomaDev <marcos.roma.dev@gmail.com>
2026-07-18 10:35:17 +0200

    Añadido obstaculo y ajustada gravedad para tener buen game feel

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ let velocityY = 0;
 let playerX = 50;
 let playerY = 50;
 let isOnGround = false;
+let obstacleX = 800;
+let obstacleY = canvas.height - 40;
 
 document.addEventListener("keydown", function (event) {
   if (event.key === " ") {
@@ -16,7 +18,7 @@ document.addEventListener("keydown", function (event) {
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  velocityY = velocityY + 0.5;
+  velocityY = velocityY + 0.25;
   playerY = playerY + velocityY;
 
   if (playerY + 40 >= canvas.height) {
@@ -26,6 +28,11 @@ function gameLoop() {
   } else {
     isOnGround = false;
   }
+
+  obstacleX = obstacleX - 2;
+
+  ctx.fillStyle = "black";
+  ctx.fillRect(obstacleX, obstacleY, 20, 40);
 
   ctx.fillStyle = "red";
   ctx.fillRect(playerX, playerY, 40, 40);
